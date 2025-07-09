bd2d0f5e83e3c75ae8720c7d561066e14b9e43a9
Agung Soeltani <agungsoeltani@gamil.com>
2025-07-09 08:48:28 +0700

    api fix

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
         widget.innerHTML = `<span style="font-size: 0.9em; color: var(--secondary-text-color);">Memuat...</span>`;
 
         try {
-            const response = await fetch('api/cuaca'); 
+            const response = await fetch('https://agungsoeltani.netlify.app/.netlify/functions/cuaca'); 
             const weatherData = await response.json();
 
             if (!weatherData || weatherData.length === 0) {

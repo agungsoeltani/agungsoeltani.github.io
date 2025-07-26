8bfd7f91c93aee9056802531503a2402d8418f37
Agung Soeltani <agungsoeltani@gamil.com>
2025-07-26 21:11:13 +0700

    simple

--- js/main.js
+++ js/main.js
@@ -21,5 +21,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     initVisitorCounter();
 
-    console.log("✅ All modules loaded bitch");
+    console.log( "All modules loaded bitch");
 });

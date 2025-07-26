ba7809a40093760a4bee0d398e271438d4700f36
Agung Soeltani <agungsoeltani@gamil.com>
2025-07-26 10:45:01 +0700

    visitor counter feature

--- js/main.js
+++ js/main.js
@@ -2,25 +2,24 @@ import { initThemeSwitcher, initCookieConsent, initCustomCursor, initScrollIndic
 import { init3dFavicon, initParticleAnimation } from '/modules/animations.js';
 import { initWeatherWidget, initDiscordPresence } from '/modules/api.js';
 import { initContactForm } from '/modules/forms.js';
+import { initVisitorCounter } from '/modules/visitorCounter.js'; 
 
-// Event listener ini memastikan semua elemen HTML sudah siap sebelum script dijalankan.
 document.addEventListener('DOMContentLoaded', () => {
-    // Inisialisasi komponen antarmuka (UI)
+
     initThemeSwitcher();
     initCookieConsent();
     initCustomCursor();
     initScrollIndicator();
 
-    // Inisialisasi animasi visual
     init3dFavicon();
     initParticleAnimation();
 
-    // Inisialisasi widget yang mengambil data dari API
     initWeatherWidget();
     initDiscordPresence();
 
-    // Inisialisasi fungsionalitas form
     initContactForm();
+
+    initVisitorCounter();
 
     console.log("✅ All modules loaded successfully!");
 });
